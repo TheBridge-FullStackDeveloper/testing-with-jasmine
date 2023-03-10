4547b0a764e6c89fe0515d67f073fd16120f98c2
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-03-10 21:35:18 +0100

    m

--- isograms/script.js
+++ isograms/script.js
@@ -1,6 +1,11 @@
 const array = [1,2,3,4,5];
 
 function minMax (){
+    let min = Math.min(...array);
+    let max = Math.max(...array)
+
+    return [min, max]
 };
 
-;
+console.log(minMax([ 6, 2, 1, 8, 10 ]));
+
--- isograms/tests/test.spec.js
+++ isograms/tests/test.spec.js
@@ -11,10 +11,21 @@ describe("Is arraySum Kats",()=>{
         expect(typeof array['length']).toEqual("number");
     })
 
-    it ("Should exist a function named minMax",()=>{
-        expect(typeof minMax).toEqual('function');
+    describe ("Function minMax",()=>{
+        it ("Should exist a function named minMax",()=>{
+            expect (typeof minMax).toEqual("function");
+        })
+        it("Result is a Array",()=>{
+            expect(minMax().length).toEqual(2)
+        })
+
+        it("Result",()=>{
+            const test = [ 6, 2, 1, 8, 10 ]; // test[0] test[1]
+            expect(test[2]).toEqual(1)
+            expect(test[4]).toEqual(10)
+
+
+        })
     })
-
-    it ("")
-
 })
+
