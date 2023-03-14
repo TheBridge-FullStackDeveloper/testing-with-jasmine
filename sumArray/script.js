61ddd844cb218ddb2189b8bc2bab19c6baf755f4
OmarjLand <omar.land2010@gmail.com>
2023-03-14 20:42:38 +0100

    Fixing and trying few things

--- sumArray/script.js
+++ sumArray/script.js
@@ -8,28 +8,38 @@
  * 1- Como quieras
  * 2- Sin el método de Array "sort" ni con "reduce"
  *
- * Datos para pruebas: input | ouput
- * sumArray(null) | 0
- * sumArray([ ]) | 0
- * sumArray([ 3 ]) | 0
- * sumArray([ 3, 5 ]) | 0
- * sumArray([ 6, 2, 1, 8, 10 ]) | 16
- * sumArray([ 0, 1, 6, 10, 10 ]) | 17
+ * Datos para pruebas: input           | ouput
+ * sumArray(null)                      | 0
+ * sumArray([ ])                       | 0
+ * sumArray([ 3 ])                     | 0
+ * sumArray([ 3, 5 ])                  | 0
+ * sumArray([ 6, 2, 1, 8, 10 ])        | 16
+ * sumArray([ 0, 1, 6, 10, 10 ])       | 17
  * sumArray([ -6, -20, -1, -10, -12 ]) | -28
- * sumArray([ -6, 20, -1, 10, -12 ]) | 3
+ * sumArray([ -6, 20, -1, 10, -12 ])   | 3
  */
 
-const customArray = [6, 2, 1, 8, 10];
+// const customArray = [6, 2, 1, 8, 10];
 
 let numMax  = 0;
 let numMin  = 0;
 let compare = 0;
+let addit   = 0;
 
 const sumArray = (array) => {
 
-    for( let i=0; i < array.length; i++ ){
-        console.log( i );
+    if( array === null || array === undefined || array.length === 1 || array.length === 2 ) {
+        return 0;
     }
+
+    for( let i=0; i < array.length; i++ ){
+        // console.log(i);
+        compare += array[i];
+    }
+
+    addit = ( compare - numMax ) - numMin;
+
+    return compare;
 
 }
 
--- sumArray/tests/indexSum.spec.js
+++ sumArray/tests/indexSum.spec.js
@@ -1,28 +1,61 @@
 describe( "SumArray function", ()=>{
 
-    it("There should be a const name customArray", () =>{
-        expect( customArray ).not.toBe(undefined);
-    })
+    // it("There should be a const name customArray", () =>{
+    //     expect( customArray ).not.toBe(undefined);
+    // })
     
     it("It should have a const named sumArray that includes a function", () =>{
         expect( sumArray ).not.toBe( undefined );
         expect( typeof sumArray ).toEqual('function');
     })
 
-    it("Need a variable named numMax", () =>{
+    it("Should initialice a variable named numMax", () =>{
         expect( numMax ).toBe(0);
     })
 
-    it("Need a variable named numMin", () =>{
+    it("Should initialice a variable named numMin", () =>{
         expect( numMin ).toBe(0);
     })
 
-    it("Need a variable named res for results", () =>{
+    it("Need a variable named compare for results", () =>{
         expect( compare ).toBe(0);
+    })
+
+    it("Need a variable named addit to make the addition for results", () =>{
+        expect( addit ).toBe(0);
     })
 
     it("It should have a for loop into the sumArray", () =>{
         expect(sumArray.toString().includes('for')).toBe(true) 
+    })
+
+    it("If array is null, i have to return 0", () =>{
+        const array = null;
+        expect( sumArray(array) ).toEqual(0)
+    })
+    
+    it("If array is undefined, i have to return 0", () =>{
+        const array = undefined;
+        expect( sumArray(array) ).toEqual(0)
+    })
+
+    it("If i get only one element i must return 0", () => {
+        const array = [0]
+        expect( sumArray(array) ).toEqual(0);
+    })
+
+    it("If i get only 2 element i must return 0", () => {
+        const array = [ 0,1 ]
+        expect( sumArray(array) ).toEqual(0);
+    })
+
+    it("Now i check each array given in the kata to evaluate and apply my funtion", () => {
+
+        expect( sumArray( [ 6, 2, 1, 8, 10  ])).toEqual(16)
+        expect( sumArray( [ 0, 1, 6, 10, 10 ])).toEqual(17)
+        expect( sumArray( [ -6, -20, -1, -10, -12 ])).toEqual(-28)
+        expect( sumArray( [ -6, 20, -1, 10, -12 ]  )).toEqual(3)
+
     })
 
     
