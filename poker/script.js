29f1fc044e7c2c12ddb2d2d13fb7d5ed2b448aa3
natal <nataliareyesvargas15@gmail.com>
2023-03-15 18:33:02 +0100

    hastaAqui

--- poker/script.js
+++ poker/script.js
@@ -1,16 +1,93 @@
 class PokerPlayer {
-  constructor() {
-    this.name
-    this.result
+  constructor(Player = '') {
+    this.name = Player
+    this.result = []
+    }
+    
+  
+
+  rollDie() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const posibilities = ['A', '8', '9', 'J', 'Q', 'K']
+        resolve(posibilities[Math.floor(Math.random() * posibilities.length)])
+        reject('Todo mal') 
+        }, 300)
+        
+      
+    
+      const fn = async () => {
+       try {
+        
+        const thatIs = await this.rollDie()
+        console.log(thatIs)
+       } catch(error) {
+         
+        console.log('que no', error)
+       } 
+      
+        
+       
+      }
+      fn()
+      
+      
+    })
+    
+    
   }
+  
 
-  rollDie() {}
+  getResult() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      for(let i = 0; i < 5; i++) {
+        if (this.rolllDie) {
+          this.result.push(throwDie[i])
+          resolve (console.log(this.result)) 
+          reject (console.log('jo'))
+        }
+      }    
 
-  getResult() {}
+        // const throwDie = this.rollDie()
+        // const loOtro = this.result.push(throwDie[i])
+        // resolve(loOtro)
+        // reject('Todo mal') 
+        }, 1500)
+      
+      
+    
+      const fn1 = async () => {
+       try {
+        
+        const esto = await this.getResult()
+        console.log(esto)
+       } catch(error) {
+         
+        console.log('que no', error)
+       } 
+        
+       
+      }
+      fn1()
+      
+     
+    })
+  }
 }
 
-const getHandName = () => {}
+const getHandName = (str = '') => {
 
-const getScore = () => {}
+return str
+}
+
+const getScore = (scr = 5) => {
+  return scr
+}
 
 const checkWinner = () => {}
+
+// const primeritoPlayer = new PokerPlayer ('Juan', []) ;
+// (primeritoPlayer.holi())
+
+
