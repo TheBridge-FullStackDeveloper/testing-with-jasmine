39331978e0062ddc014f397f4bd435ae63fff76a
natal <nataliareyesvargas15@gmail.com>
2023-03-15 21:27:32 +0100

    hastaGetResult

--- poker/script.js
+++ poker/script.js
@@ -7,81 +7,44 @@ class PokerPlayer {
   
 
   rollDie() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         const posibilities = ['A', '8', '9', 'J', 'Q', 'K']
         resolve(posibilities[Math.floor(Math.random() * posibilities.length)])
-        reject('Todo mal') 
         }, 300)
+        
         
       
     
-      const fn = async () => {
-       try {
-        
-        const thatIs = await this.rollDie()
-        console.log(thatIs)
-       } catch(error) {
-         
-        console.log('que no', error)
-       } 
+     
       
-        
        
-      }
-      fn()
-      
-      
     })
     
     
   }
   
 
-  getResult() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-      for(let i = 0; i < 5; i++) {
-        if (this.rolllDie) {
-          this.result.push(throwDie[i])
-          resolve (console.log(this.result)) 
-          reject (console.log('jo'))
-        }
-      }    
+  async getResult() {
 
-        // const throwDie = this.rollDie()
-        // const loOtro = this.result.push(throwDie[i])
-        // resolve(loOtro)
-        // reject('Todo mal') 
-        }, 1500)
-      
-      
-    
-      const fn1 = async () => {
-       try {
-        
-        const esto = await this.getResult()
-        console.log(esto)
-       } catch(error) {
-         
-        console.log('que no', error)
-       } 
-        
-       
-      }
-      fn1()
-      
-     
-    })
+        const throwDie = await this.rollDie()
+         for(let i = 0; ++i < 6;) {
+         if(this.result.length < 5) {
+          this.result.push(throwDie);
+          }
+        }
+
+        console.log(this.result)
+
   }
 }
-
+ 
 const getHandName = (str = '') => {
 
 return str
 }
 
-const getScore = (scr = 5) => {
+const getScore = (scr = 17) => {
   return scr
 }
 
@@ -90,4 +53,52 @@ const checkWinner = () => {}
 // const primeritoPlayer = new PokerPlayer ('Juan', []) ;
 // (primeritoPlayer.holi())
 
+ // const fn = async () => {
+      //  try {
+        
+      //   const thatIs = await this.rollDie()
+      //   console.log(thatIs)
+      //  } catch(error) {
+         
+      //   console.log('que no', error)
+      //  } 
+      
+        
+       
+      // }
+      // fn()
 
+  //     const fn1 = async () => {
+  //      try {
+        
+  //       const esto = await this.getResult()
+  //       console.log(esto)
+  //      } catch(error) {
+         
+  //       console.log('que no', error)
+  //      } 
+        
+       
+  //     }
+  //     fn1()
+      
+     
+  //   })
+  // }
+
+      //   if (throwDie) {
+          
+      //     for(let i = 0; i < 5; i++) {
+            
+      //       this.result.push(throwDie[i])
+          
+          
+      //   }
+      // }    
+      
+
+        // const throwDie = this.rollDie()
+        // const loOtro = this.result.push(throwDie[i])
+        // resolve(loOtro)
+        // reject('Todo mal') 
+      
